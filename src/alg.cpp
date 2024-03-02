07995fc6a91e69e231761fbb6939dd8ff72c8b56
vladudin1 <160415615+vladudin1@users.noreply.github.com>
2024-03-02 14:29:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,72 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (int i = 2; i < value; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int count = 0;
+  int num = 2;
+  while (true) {
+      int del = 2;
+      int max_del = num / 2;
+      bool is_prime = true;
+      while (del <= max_del) {
+          if (num % del == 0) {
+              is_prime = false;
+              break;
+          }
+          del++;
+      }
+      if (is_prime) {
+          count++;
+          if (count == n)
+              return num;
+      }
+      num++;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 1) {
+      return 2;
+  }
+
+  uint64_t num = (value % 2 == 0) ? value + 1 : value + 2;
+
+  while (true) {
+      bool isPrime = true;
+      for (uint64_t i = 2; i * i <= num; ++i) {
+          if (num % i == 0) {
+              isPrime = false;
+              break;
+          }
+      }
+      if (isPrime) {
+          return num;
+      }
+      num += 2;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; ++i) {
+  bool isPrime = true;
+      for (uint64_t j = 2; j * j <= i; ++j) {
+          if (i % j == 0) {
+              isPrime = false;
+              break;
+          }
+      }
+      if (isPrime) {
+          sum += i;
+      }
+  }
+  return sum;
 }
